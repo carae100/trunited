8b6fcb7973cea35d9821a1c7c5557d3dabb93e7a
Dracosomething <larsfalk08@gmail.com>
2025-01-18 17:02:59 +0100

    quick lil update

--- changelog.md
+++ changelog.md
@@ -1,18 +1,41 @@
-# changelog
-updates
-- changed voice of the honkai's border color
-- gave all text colors
+# Azazel, Lord of Flesh
 
-new
-- added starkill
-- added concept of infinity
-- added Azazel, Lord of Flesh
-- added Akashic Plane
+[passive] Knowledge
+player is immune to both brain damage effect and M.A.D. effect.
 
-starkill obtained from reincarnation
+[active] Infect\
+change modes by shifting and pressing the ability key\
+Infect modes:
 
-starkill evolves into azazel when starkill is mastered, the user is a demon lord seed or hero egg and the user has asimilated 300 entities after mastery
+melt\
+gives all entities in a 15 block radius the melt effect\
+melt decreases ones max hp the longer you have it. Can not be removed with reverser or 
+isolation, has a 10% chance to be removed with milk.
 
-concept of infinity obtainable by becoming a demon lord seed or hero egg, mastering sage and not having either starkill or azazel
+apostle creation\
+makes everything in a 15 block radius your subordinate. Makes a clone of players.
 
-akashic plane can be obtained by mastering concept of infinity, being a tdl or true hero and having 1 million ep and not having either starkill or azazel
+
+[active] assimilation\
+Basically gluttony breath in a 15 block radius. every killed entity gives a assimilation point.
+
+[active] Akashic Plane\
+has 4 modes, switch modes by shift and pressing the ability key
+
+analyze\
+normal analyze
+
+Overwhelm\
+gives all entities in a 15 block radius the brain damage effect
+
+spatial cut\
+deals 25 damage to a target, also stops their healing
+
+podolsky neurons\
+gives a command to all subordinates in a 60 block radius.\
+commands can be changed by sprinting, holding the ability key and scrolling(shift was already taken)
+
+[active] create apostle\
+uses a assimilation point to summon a random entity from this list.\
+EntityType.ZOMBIE, EntityType.PIGLIN_BRUTE, EntityType.SKELETON, EntityType.ZOMBIFIED_PIGLIN, EntityType.ZOMBIE_VILLAGER, EntityType.ZOGLIN, EntityType.CREEPER, EntityType.EVOKER, EntityType.PIGLIN_BRUTE, EntityType.GUARDIAN, EntityType.ELDER_GUARDIAN, EntityType.ILLUSIONER, EntityType.PILLAGER, EntityType.VINDICATOR, EntityType.SILVERFISH, EntityType.RAVAGER, EntityType.HUSK, EntityType.DROWNED, EntityType.HOGLIN, EntityType.PIGLIN, EntityType.SPIDER, EntityType.CAVE_SPIDER, EntityType.PHANTOM, EntityType.STRAY, TensuraEntityTypes.BARGHEST.get(), TensuraEntityTypes.ARMOURSAURUS.get(), TensuraEntityTypes.BLACK_SPIDER.get(), TensuraEntityTypes.KNIGHT_SPIDER.get()\
+the summoned entity is your subordinate.
--- src/main/java/io/github/dracosomething/trawakened/mixin/RaceSelectionMenuMixin.java
+++ src/main/java/io/github/dracosomething/trawakened/mixin/RaceSelectionMenuMixin.java
@@ -9,11 +9,13 @@ import com.github.manasmods.tensura.config.TensuraConfig;
 import com.github.manasmods.tensura.menu.RaceSelectionMenu;
 import com.github.manasmods.tensura.world.TensuraGameRules;
 import com.github.manasmods.tensura.world.savedata.UniqueSkillSaveData;
+import io.github.dracosomething.trawakened.registry.skillregistry;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Final;
@@ -33,15 +35,13 @@ import static com.github.manasmods.tensura.menu.RaceSelectionMenu.getSecondReinc
 
 @Mixin(RaceSelectionMenu.class)
 public abstract class RaceSelectionMenuMixin {
-    @Shadow @Final private Player player;
-
     @Inject(
             method = "getReincarnationSkills",
             at = @At(value = "HEAD"),
             remap = false,
             cancellable = true)
-    private static void NewUniques(ServerLevel level, boolean ignoreGameRule, CallbackInfoReturnable<List<ManasSkill>> cir) {
-        List<ManasSkill> skills = new ArrayList<ManasSkill>(((List) TensuraConfig.INSTANCE.skillsConfig.reincarnationSkills.get()).stream().filter((skill) -> {
+    private static void NewUniques(ServerLevel level, boolean ignoreGameRule, Player roller, CallbackInfoReturnable<List<ManasSkill>> cir) {
+        List<ManasSkill> skills = new ArrayList<ManasSkill>(((List)TensuraConfig.INSTANCE.skillsConfig.reincarnationSkills.get()).stream().filter((skill) -> {
             if (ignoreGameRule) {
                 return true;
             } else if (!level.getGameRules().getBoolean(TensuraGameRules.TRULY_UNIQUE)) {
@@ -50,9 +50,16 @@ public abstract class RaceSelectionMenuMixin {
                 return !UniqueSkillSaveData.get(level.getServer().overworld()).hasSkill(new ResourceLocation(skill.toString()));
             }
         }).map((skill) -> {
-            return (ManasSkill) SkillAPI.getSkillRegistry().getValue(new ResourceLocation(skill.toString()));
-        }).filter(Objects::nonNull).toList());
+            return (ManasSkill)SkillAPI.getSkillRegistry().getValue(new ResourceLocation(skill.toString()));
+        }).filter((skill) -> {
+            if (skill == null) {
+                return false;
+            } else {
+                return roller == null || !SkillUtils.hasSkill(roller, (ManasSkill) skill);
+            }
+        }).toList());
         skills.add(SkillAPI.getSkillRegistry().getValue(new ResourceLocation("trawakened:starkill")));
+        skills.add(skillregistry.AKASHIC_PLANE.get());
         System.out.println(skills);
         cir.setReturnValue(skills);
 //        List<ManasSkill> list = new ArrayList<>(List.of());
